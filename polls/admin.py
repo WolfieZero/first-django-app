9881585f0b89f829ba2c8d5be323ae310b1fa4f3
WolfieZero <neil@wolfiezero.com>
2016-11-24 10:00:48 +0000

    Completed part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 10
+
+admin.site.register(Question, QuestionAdmin)
